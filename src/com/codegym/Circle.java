9ac387c48484dfdc29aa8e4f5b88be839652c9db
ruukichi <ngovietlc93@gmail.com>
2022-01-10 17:13:43 +0700

    access modifier

---
+++ src/com/codegym/Circle.java
@@ -0,0 +1,34 @@
+package com.codegym;
+
+public class Circle {
+    private double radius = 1.0 ;
+    private String color = "red";
+
+    public Circle() {
+
+    }
+
+    public Circle(double radius) {
+
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.pow(this.radius, 2) * Math.PI;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ src/com/codegym/Main.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Circle circle1 = new Circle();
+        Circle circle2 = new Circle();
+        circle2.setRadius(5);
+        System.out.println(circle1.getArea());
+        System.out.println(circle2.getArea());
+
+    }
+}
